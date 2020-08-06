721e0842d868a7eb6b2234db0a2b9233c63ef51c
aris-osorio <aris.osorio@alumnos.udg.mx>
2020-08-06 18:48:27 -0500

    restructuring App.js (component function whith hooks)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Principal from './components/principal'
 import Loading from './components/loading'
 import './App.css';
 
 
-export default class App extends React.Component 
+export default function App()
 {
-  constructor()
-  {
-    super();
-    this.state = {window : "loading"};
-  }
+  const [window, setWindows] = useState("Loading");
+  let showWindow;
 
-  componentDidMount() 
+  useEffect(() => 
   {
-    setTimeout(() => this.setState({ window: "principal" }), 2000);
-  }
+    const timer = setTimeout(() => {
+      setWindows("Principal")
+    }, 2000);
 
-  render()
+    return () => clearTimeout(timer);
+
+  }, [])
+
+  if(window ==="Loading")
   {
-    switch(this.state.window)
-    {
-      case "loading":
-        return (
-          <div className="vh-100 d-flex justify-content-center align-items-center">
-            <Loading />
-          </div>
-        );
-      case "principal":
-        return (
-            <div className="vh-100 bg-principal">
-              <div className="vh-100 d-flex justify-content-center align-items-center position-relative bg-opacity">
-                <Principal />
-              </div>
-            </div>
-        );
-    }
+    showWindow = (
+                    <div className="vh-100 d-flex justify-content-center align-items-center">
+                      <Loading />
+                    </div>
+                  )
+  }
+  else if(window ==="Principal")
+  {
+    showWindow = (
+                    <div className="vh-100 bg-principal">
+                      <div className="vh-100 d-flex justify-content-center align-items-center position-relative bg-opacity">
+                        <Principal />
+                      </div>
+                    </div>
+                  )
   }
   
+  return (
+    <div>
+      {showWindow}
+    </div>
+  )
+   
 }
 
 
